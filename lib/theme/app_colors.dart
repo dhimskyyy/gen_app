f89b9087b6eeabc10a64d9115923a31b440e886a
Mohammad Dhimas Afrizal <mdhimas25@gmail.com>
2025-07-31 22:47:03 +0700

    update color

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF498DEC);
+  static const Color primary = Color(0xFF1A68D4);
   static const Color white = Colors.white;
   static const Color black = Colors.black;
-  static const Color black87 = Colors.black87;
-  static const Color black54 = Colors.black54;
-  static const Color black26 = Colors.black26;
   static final Color lightGrey = Colors.grey.shade300;
 
-  // Soft Colors with Opacity (10%)
   static const Color greenSoft = Color(0x1A4CAF50);
   static const Color yellowSoft = Color(
     0x1AFFEB3B,
-  ); // Yellow soft (10% opacity)
+  );
   static const Color redSoft = Color(0x1AF44336);
 
   // Shadow Soft (5% opacity)
@@ -22,6 +18,4 @@ class AppColors {
   // Delete Red Color
   static const Color deleteRed = Color(0xFFF44336);
 
-  // Text Primary Color
-  static const Color textPrimary = Color(0xFF498DEC); // Sama dengan primary
 }
